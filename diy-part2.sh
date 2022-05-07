8b686ecc8da3d75bf38a274c83f2096829dbc8b4
a1897 <a1897@liqunlock.ca>
2022-05-06 23:02:28 -0400

    change default ip

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,7 +8,7 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.4.16.246/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.4.16.247/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 sed -i 's/OpenWrt/ac7v2/g' package/base-files/files/bin/config_generate
